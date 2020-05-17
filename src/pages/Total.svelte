36b978a6f5976f59207c6dd6c752bd83cd4d3166
John Pangilinan <john.pangilinan1@gmail.com>
2020-05-17 10:02:17 -0700

    Some refactoring

--- src/pages/Total.svelte
+++ src/pages/Total.svelte
@@ -10,14 +10,53 @@ import { currentPage, pageTitle, totalPageModel } from '../stores.js';
 // presentational components    
 import Tab, {Icon, Label} from '@smui/tab';
 import TabBar from '@smui/tab-bar';
+import Dialog, {Content, Actions, Title} from '@smui/dialog';
+import LinearProgress from '@smui/linear-progress';
+
+
 import DisplayTable from '../components/DisplayTable.svelte';
 
 import MapChart from '../components/MapChart.svelte';
 
 export let params;
 
-let activeTab = 'Confirmed Cases (daily)';
-let types = {
+// Map related Data
+const mapTypes = {
+    'death': {
+        attribute: 'death',
+        title: 'Total Deaths by State',
+        color: 'orange'
+    },
+    'positive': {
+        attribute: 'positive',
+        title: 'Total Confirmed Positives by State',
+        color: 'blue'
+    },
+    'totalTestResults': {
+        attribute: 'totalTestResults',
+        title: 'Total Tests by State',
+        color: 'green'
+    },        
+    'deathIncrease': {
+        attribute: 'deathIncrease',
+        title: 'Daily Mortality',
+        color: 'orange'
+    },        
+    'positiveIncrease': {
+        attribute: 'positiveIncrease',
+        title: 'Daily Mortality',
+        color: 'blue'
+    },        
+    'totalTestResultsIncrease': {
+        attribute: 'totalTestResultsIncrease',
+        title: 'Daily Tests',
+        color: 'green'
+    }   
+};
+let curMapType = 'positiveIncrease';
+
+// Tab Related Data
+let tabTypes = {
     'Confirmed Cases (daily)': 'positiveIncrease',
     'Mortalities (daily)': 'deathIncrease',
     'Tested (daily)': 'totalTestResultsIncrease',
@@ -26,28 +65,32 @@ let types = {
     'Mortalities': 'death',
     'Tested': 'totalTestResults'
 };
-let tabs = Object.keys(types);
-let curType = 'positive';
+let activeTab = 'Confirmed Cases (daily)';
+let tabs = Object.keys(tabTypes);
 
 // Use LA Time to get dateString, certain areas of the world can be ahead of us and throw off the map
-let dateString = moment()
-//    .tz(moment.tz.guess())
+let curDate = moment()
     .tz('America/Los_Angeles')
-    .subtract(1, 'days')
-    .format('YYYYMMDD');
-let mapJson = {};
+    .subtract(1, 'days');
+let dateString = curDate.format('YYYYMMDD');
 
+// source data, retrieved from localStorage or the API
 let covidData = [];
+
+// derived data, for the Map and Table components, respectively
+let mapJson = {};
 let tableData = {};
 
-// For table sorting
+// Variables to control the sorting on the page, for column sort and direction
 let activeColumn = 'state';
 let curDir = 'asc';
 
+// local dialog variables
+let dialogRef;
+let dialogTitle = '';
+let dialogContent = '';
 
-let response;
-
-// Reactive statements for headers & tableData
+// Set the active column and direction from the params
 $: { 
     if(params) {
         let { sort, dir } = params;
@@ -56,111 +99,107 @@ $: {
     }
 };
 
-$: { 
-    if(covidData && covidData.data) {
-        tableData = { 
-            headers: covidData.headers.slice(),
-            data: covidData.data.slice()
-        };
-
-        let totals = covidData.data.reduce( (acc, cur) => {
-            let keys = Object.keys(acc);
-
-            for(let key of keys) {
-                acc[key] += cur[key];
-            }
+// Table Data: Create the total row and the Table data from covidData
+$: {
+    if(covidData != undefined && covidData.data != undefined) {
+        let newData = covidData.data.slice();
+        newData = newData.sort(sortTableData(activeColumn, curDir) );
         
-            return acc;
-        }, { positive: 0, death: 0, totalTestResults: 0, pending: 0,
-            positiveIncrease: 0, deathIncrease: 0, totalTestResultsIncrease: 0, pendingIncrease: 0
-        });
-        totals = {
-            positive: `${formatNumber(totals.positive)} (${formatNumber(totals.positiveIncrease)})`,
-            death: `${formatNumber(totals.death)} (${formatNumber(totals.deathIncrease)})`,
-            totalTestResults: `${formatNumber(totals.totalTestResults)} (${formatNumber(totals.totalTestResultsIncrease)})`,
-            posPercentage: '',
-            deathPercentage: '',
-            hospitalizedCurrently: '',
-            inIcuCurrently: '',
-            onVentilatorCurrently: '',
-            pending: formatNumber(totals.pending)
-        };
-        totals = { state: 'Totals', ...totals }; 
-
-        tableData.data = tableData.data.sort(sortData(activeColumn, curDir) );
-        tableData.data = tableData.data.map(row => {
-            let newObj = {};
-            for(let prop in row) {
-                if(!['stateName', 'country', 'positiveIncrease', 'deathIncrease', 'totalTestResultsIncrease']
-                    .includes(prop) ) { 
-                        newObj[prop] = (isNaN(row[prop])) ? row[prop] : formatNumber(row[prop]); 
-                    }
-            }
-
-            newObj['state'] = { href: `#/state/${row.state}/`, text: row.stateName };
-
-            newObj['positive'] += ` (${formatNumber(row.positiveIncrease)})`;
-            newObj['death'] += ` (${formatNumber(row.deathIncrease)})`; 
-            newObj['totalTestResults'] += ` (${formatNumber(row.totalTestResultsIncrease)})`; 
-
-            return newObj;
-        });
-        tableData.data = [totals, ...tableData.data];
+        tableData = getTableData(covidData.headers, newData);
     }
 }
+    
+// Map Data: Whenever the curMapType changes, get new mapJson data
+$: mapJson = getMapData(covidData.data, curMapType);
 
-const formatNumber = number => new Intl.NumberFormat().format(number);
+const getMapData = (theData, theMapType) => {
+    if(theData == undefined) { console.error(`[getMapData] No map data provided`); return; }
 
-const getMapData = (theData, type) => {
-    let mapFunc = () => {};
-    let color = '';
-    let title = '';
-    switch(type) {
-        case 'death':
-            mapFunc = state => ([state.stateName, state.death]);
-            title = 'Total Deaths by State';
-            color = 'orange';
-            break;
-        case 'positive':
-            mapFunc = state => ([state.stateName, state.positive]);
-            title = 'Total Confirmed Positives by State';
-            color = 'blue';
-            break;
-        case 'totalTestResults':
-            mapFunc = state => ([state.stateName, state.totalTestResults]);
-            title = 'Total Tests by State';
-            color = 'green';
-            break;
-        case 'deathIncrease':
-            mapFunc = state => ([state.stateName, state.deathIncrease]);
-            title = 'Daily Mortality';
-            color = 'orange';
-            break;
-        case 'positiveIncrease':
-            mapFunc = state => ([state.stateName, state.positiveIncrease]);
-            title = 'Daily Confirmed Cases';
-            color = 'blue';
-            break;
-        case 'totalTestResultsIncrease':
-            mapFunc = state => ([state.stateName, state.totalTestResultsIncrease]);
-            title = 'Daily Tests';
-            color = 'green';
-            break;
-    }
+    // mapFunc for creating new map data
+    // Format: [stateAbbreviation, attribute]
+    // I.e. ['CA', 5] for [state, numberCases]
+    let mapFunc = attr => state => ([state['stateName'], state[attr]]);
 
-    return Object.assign( new Map( theData.map(mapFunc) ), { title, color } );
+    // attribute will be provided by the current map type
+    let { color, title, attribute } = mapTypes[theMapType];
+
+    return Object.assign( new Map( theData.map(mapFunc(attribute) ) ), { title, color } );
 };
 
-const sortData = (column, direction) => (first, second) => {
+const sortTableData = (column, direction) => (first, second) => {
     let multiplier = (direction === 'asc') ? 1 : -1;
     return (first[column] < second[column]) ? multiplier * -1 : multiplier * 1;
 };
+
+const getTableData = (theHeaders, theData, newCol, newDir) => {
+    if(theHeaders == undefined || theData == undefined) 
+    { console.error(`[getTableData] No headers/data provided`); return; }
+
+    // get the tableData and create new references
+    let newData = { 
+        headers: theHeaders.slice(),
+        data: theData.slice()
+    };
+
+    // create the totals row for the table
+    // aggregate the supplied columns
+    // order of columns is important, which is why it's initialized
+    let totals = theData.reduce( (acc, cur) => {
+        let keys = Object.keys(acc);
+        for(let key of keys) { acc[key] += cur[key]; }
+        return acc;
+    }, { positive: 0, death: 0, totalTestResults: 0, pending: 0,
+        positiveIncrease: 0, deathIncrease: 0, totalTestResultsIncrease: 0, pendingIncrease: 0
+    });
+
+    // pretty up the output 
+    totals = {
+        positive: `${formatNumber(totals.positive)} (${formatNumber(totals.positiveIncrease)})`,
+        death: `${formatNumber(totals.death)} (${formatNumber(totals.deathIncrease)})`,
+        totalTestResults: `${formatNumber(totals.totalTestResults)} (${formatNumber(totals.totalTestResultsIncrease)})`,
+        posPercentage: '',
+        deathPercentage: '',
+        hospitalizedCurrently: '',
+        inIcuCurrently: '',
+        onVentilatorCurrently: '',
+        pending: formatNumber(totals.pending)
+    };
+    totals = { state: 'Totals', ...totals }; 
+
+    // create the table data
+    newData.data = newData.data.map(row => {
+        let newObj = {};
+        for(let prop in row) {
+            if(!['stateName', 'country', 'positiveIncrease', 'deathIncrease', 'totalTestResultsIncrease']
+                .includes(prop) ) { 
+                    newObj[prop] = (isNaN(row[prop])) ? row[prop] : formatNumber(row[prop]); 
+                }
+        }
+
+        let { state, stateName, positiveIncrease, deathIncrease, totalTestResultsIncrease } = row;
+
+        newObj['state'] = { href: `#/state/${state}/`, text: stateName };
+
+        newObj['positive'] += ` (${formatNumber(positiveIncrease)})`;
+        newObj['death'] += ` (${formatNumber(deathIncrease)})`; 
+        newObj['totalTestResults'] += ` (${formatNumber(totalTestResultsIncrease)})`; 
+
+        return newObj;
+    });
+    // put the totals row in the first row of the Table dataset
+    newData.data = [totals, ...newData.data];
+
+    return newData;
+};
+
+// Helper function for formatting numbers
+const formatNumber = number => new Intl.NumberFormat().format(number);
 
 // Handles the sorting that occurs when a user clicks on the Header
 const dispatchHandlerHeaderClick = e => {
 
     let target = e.detail.target;
-    if(!target) { console.error('[Total - dispatchHandlerHeaderClick] Unable to sort', event.target); return; }
+    if(!target) { console.error('[Total - dispatchHandlerHeaderClick] Unable to sort', e.target); return; }
 
     let index = target.dataset.index;
     let newColumn = target.dataset.sort;
@@ -174,36 +213,54 @@ const dispatchHandlerHeaderClick = e => {
     push(`/sort/${activeColumn}/${curDir}`);
 };
 
+// Stub for Row Clicks on the Table
 const dispatchHandlerRowClick = e => {
     let target = e.detail.target;
     if(!target) { console.error('Unable to sort', event.target); return; }
-
 };
 
-const handlerClick = (e) => {
-    curType = types[activeTab];
-    mapJson = getMapData(covidData.data, curType);
-
-    totalPageModel.setMapType(curType);
+// handlerTabClick will trigger a map change by changing curMapType
+const handlerTabClick = (e) => {
+    curMapType = tabTypes[activeTab];
+    totalPageModel.setMapType(curMapType);
     totalPageModel.setActive(activeTab);
 };
 
-onMount(async () => {
-	let response;
+const handlerDialogClosed = function(e) { };
 
-    pageTitle.set('Totals');
+
+onMount(async () => {
+    dialogTitle = "Please wait";
+    dialogContent = "Loading";
+    
+    // There's some sort of focus bug when opening up the dialog sometimes, what could be going on here?
+    try { dialogRef.open(); }
+    catch(e) { console.error(e); }
+
+    pageTitle.set(`Totals for ${curDate.format('M/DD/YYYY')}`);
     currentPage.set('total');
 
-    covidData = await model.get({ type: 'total', dateString });
-
     if(!$totalPageModel.mapType || $totalPageModel.mapType.length <= 0) { 
-        totalPageModel.setMapType(curType); 
+        totalPageModel.setMapType(curMapType); 
     }
     else {
-        curType = $totalPageModel.mapType;
+        curMapType = $totalPageModel.mapType;
     }
     activeTab = $totalPageModel.active || activeTab;
-    mapJson = getMapData(covidData.data, curType);
+
+    // Fetch the data
+    try {
+        covidData = await model.get({ type: 'total', dateString });
+        dialogRef.close();
+    }
+    catch(e) {
+        dialogTitle = "Error";
+        dialogContent = "There was an error loading the page, please try reloading your browser";
+
+        console.error('There was an error fetching the data', e);
+    }
+
+    mapJson = getMapData(covidData.data, curMapType);
 });
 </script>
 
@@ -214,11 +271,16 @@ onMount(async () => {
         display: flex;
         flex-direction: column; 
     }
+    .content :global(.mdc-data-table th:hover) {
+        cursor: pointer;
+    }
+
     .charts { 
         display: flex;
         flex-direction: column;
         max-height: 40vh;
     }
+
 
 	@media (min-width: 640px) {
         .content {
@@ -232,14 +294,13 @@ onMount(async () => {
 </style>
 
 <svelte:head>
-    <title>CV Totals for the US</title>
+    <title>CV Totals for the US ({ curDate.format('M/DD/YYYY') }) </title>
 </svelte:head>
 
+{#if Object.keys(covidData).length > 0}
 <section class="content">
-    <!-- <h1>Covid Data for <span>{ moment(dateString).format('MM/DD/YYYY')}</span></h1> -->
-
     <section class="charts">
-        <TabBar tabs={tabs} let:tab bind:active={activeTab} on:click={handlerClick}>
+        <TabBar tabs={tabs} let:tab bind:active={activeTab} on:click={handlerTabClick}>
             <!-- Notice that the `tab` property is required! -->
             <Tab {tab}>
                 <Label>{tab}</Label>
@@ -252,5 +313,16 @@ onMount(async () => {
     on:headerClick={dispatchHandlerHeaderClick} 
     on:rowClick={dispatchHandlerRowClick} 
     />
-
 </section>
+{/if}
+
+<Dialog bind:this={dialogRef} 
+    aria-labelledby="simple-title" 
+    aria-describedby="simple-content"
+    on:MDCDialog:closed={handlerDialogClosed}
+>
+    <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+    <Title id="simple-title">{dialogTitle}</Title>
+    <Content id="simple-content">{dialogContent}</Content>
+    <LinearProgress indeterminate />
+</Dialog>
